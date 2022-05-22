cb20a0dc4f38fe6e51aa6569510999f11e677abf
KrisztinaKomocsin <krisztina.komocsin@gmail.com>
2022-05-22 20:18:46 +0200

    Add first version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
+/* eslint-disable react/hook-use-state */
 import './App.css';
+import React, { useEffect, useState } from 'react';
+
+const baseUrl = 'http://localhost:4000';
 
 function App() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  useEffect(() => {
+    async function getGuests() {
+      const response = await fetch(`${baseUrl}/guests`);
+      const allGuests = await response.json();
+      const cleanedUsers = await allGuests.map((user) => {
+        return {
+          name: user.firstName.first,
+          surname: user.lastName.last,
+          id: user.id,
+          attending: user.attending,
+        };
+      });
+      setGuests(cleanedUsers);
+    }
+    getGuests();
+  }, []);
+
   return (
     <div className="App">
       <h1>GUEST LIST</h1>
-      <div className="Form">
-        <div className="guestName">
+      <form
+        className="guest-list"
+        onSubmit={(event) => {
+          event.preventDefault();
+        }}
+      >
+        <div>
           <input
             placeholder="First Name"
-            //value={firstName}
-            // onChange={(event) => setTopText(event.currentTarget.value)}
+            value={firstName}
+            onChange={(event) => setFirstName(event.currentTarget.value)}
           />
           <input
             placeholder="Last Name"
-            //value={lastName}
-            //onChange={(event) => setBottomText(event.currentTarget.value)}
+            value={lastName}
+            onChange={(event) => setLastName(event.currentTarget.value)}
           />
-          <button>Return</button>
-          <button>Remove</button>
+          <button className="button-add">Add guest</button>
+          <button>Remove guest</button>
+          <input
+            aria-label={`${guest.firstName} ${guest.lastName} ${guest.attending}`}
+          />
         </div>
-      </div>
-    </div>gi
+      </form>
+    </div>
   );
 }
 
